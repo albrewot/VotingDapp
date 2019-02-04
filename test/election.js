727f4e0c387cd65d37d53427353d3b7c9238be77
Albert Torres <albrewot@gmail.com>
2019-02-04 01:50:16 -0400

    Finished Project Commit

---
+++ test/election.js
@@ -0,0 +1,108 @@
+const Election = artifacts.require("./Election.sol");
+let electionInstance = 0;
+
+contract("Election", accounts => {
+  it("initializes with two candidates", async () => {
+    return Election.deployed()
+      .then(instance => {
+        return instance.candidatesCount();
+      })
+      .then(count => {
+        assert.equal(count, 2);
+      });
+  });
+
+  it("initializes candidates with correct values", () => {
+    return Election.deployed()
+      .then(instance => {
+        electionInstance = instance;
+        return electionInstance.candidates(1);
+      })
+      .then(candidate => {
+        assert.equal(candidate[0], 1, "contains correct id");
+        assert.equal(candidate[1], "Candidate 1", "contains correct name");
+        assert.equal(candidate[2], 0, "contains correct count vote");
+        return electionInstance.candidates(2);
+      })
+      .then(candidate => {
+        assert.equal(candidate[0], 2, "contains correct id");
+        assert.equal(candidate[1], "Candidate 2", "contains correct name");
+        assert.equal(candidate[2], 0, "contains correct count vote");
+      });
+  });
+
+  it("allows a voter to cast a vote", () => {
+    return Election.deployed()
+      .then(instance => {
+        electionInstance = instance;
+        candidateId = 1;
+        return electionInstance.vote(candidateId, { from: accounts[0] });
+      })
+      .then(receipt => {
+        assert.equal(receipt.logs.length, 1, "An event was triggered");
+        assert.equal(receipt.logs[0].event, "votedEvent", "The event type is correct");
+        assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
+        return electionInstance.voters(accounts[0]);
+      })
+      .then(voted => {
+        assert(voted, "the voter was marked as voted");
+        return electionInstance.candidates(candidateId);
+      })
+      .then(candidate => {
+        const voteCount = candidate[2];
+        assert.equal(voteCount, 1, "increments the candidate's vote count");
+      });
+  });
+
+  it("throws an exception for invalid candidates", () => {
+    return Election.deployed()
+      .then(instance => {
+        electionInstance = instance;
+        return electionInstance.vote(99, { from: accounts[1] });
+      })
+      .then(assert.fail)
+      .catch(error => {
+        assert(error.message.indexOf("revert") >= 0, "error message must contain revert");
+        return electionInstance.candidates(1);
+      })
+      .then(candidate1 => {
+        var voteCount = candidate1[2];
+        assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+        return electionInstance.candidates(2);
+      })
+      .then(candidate2 => {
+        var voteCount = candidate2[2];
+        assert.equal(voteCount, 0, "candidate 2 did not receive any votes");
+      });
+  });
+
+  it("throws an exception for double voting", () => {
+    return Election.deployed()
+      .then(instance => {
+        electionInstance = instance;
+        candidateId = 2;
+        electionInstance.vote(candidateId, { from: accounts[1] });
+        return electionInstance.candidates(candidateId);
+      })
+      .then(candidate => {
+        var voteCount = candidate[2];
+        assert.equal(voteCount, 1, "accepts first vote");
+        // Try to vote again
+        return electionInstance.vote(candidateId, { from: accounts[1] });
+      })
+      .then(assert.fail)
+      .catch(error => {
+        assert(error.message.indexOf("revert") >= 0, "error message must contain revert");
+        return electionInstance.candidates(1);
+      })
+      .then(candidate1 => {
+        var voteCount = candidate1[2];
+        assert.equal(voteCount, 1, "candidate 1 did not receive any votes");
+        return electionInstance.candidates(2);
+      })
+      .then(candidate2 => {
+        var voteCount = candidate2[2];
+        assert.equal(voteCount, 1, "candidate 2 did not receive any votes");
+      });
+  });
+});
